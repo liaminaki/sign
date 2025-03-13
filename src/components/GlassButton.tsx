cd37f98e561cbf8c517fa97e134eded85da5b84b
Liam Iñaki Gillamac <yakogillamac96@gmail.com>
2025-03-13 18:11:41 +0800

    Edit GlassButton component for onClick event

--- src/components/GlassButton.tsx
+++ src/components/GlassButton.tsx
@@ -3,13 +3,14 @@ import '../glass.css';
 import '../index.css';
 
 type GlassProps = {
+    onClick?: () => void;
     children: ReactNode;
 };
 
-const Glass: React.FC<GlassProps> = ({ children }) => {
+const Glass: React.FC<GlassProps> = ({ onClick, children }) => {
     return (
         <div className='glass-container'>
-            <button className="glass">
+            <button className="glass" onClick={onClick}>
                 <div className="glass-stroke"></div>
                 <div className="glass-content">{children}</div>
             </button>
