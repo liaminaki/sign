0b8cd2f42f3a3aab6500c0c943562c9d9a87a995
Liam Iñaki Gillamac <yakogillamac96@gmail.com>
2025-03-14 01:11:05 +0800

    Allow GlassButton to accept className
    This is to determine the size of the card or the button.

--- src/components/GlassButton.tsx
+++ src/components/GlassButton.tsx
@@ -5,11 +5,12 @@ import '../index.css';
 type GlassProps = {
     onClick?: () => void;
     children: ReactNode;
+    className?: string;
 };
 
-const Glass: React.FC<GlassProps> = ({ onClick, children }) => {
+const Glass: React.FC<GlassProps> = ({ onClick, children, className }) => {
     return (
-        <button className="glass-card" onClick={onClick}>
+        <button className={`glass-card ${className}`} onClick={onClick}>
             <div className="glass-stroke"></div>
             <div className="glass-content">{children}</div>
         </button>
