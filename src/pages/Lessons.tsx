28c1c2df9e3519dc28edcf0dcfb11fd02d46152c
Liam Iñaki Gillamac <yakogillamac96@gmail.com>
2025-03-14 16:11:37 +0800

    Add Lessons Page

--- src/pages/Lessons.tsx
+++ src/pages/Lessons.tsx
@@ -1,23 +1,42 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import GlassButton from '../components/GlassButton';
+import icon1 from '../assets/img/alphabet.svg';
+import icon2 from '../assets/img/numbers.svg';
+import icon3 from '../assets/img/greetings.svg';
 
 const Lessons: React.FC = () => {
+  const navigate = useNavigate();
+
   const lessons = [
-    { id: 1, title: 'Lesson 1' },
-    { id: 2, title: 'Lesson 2' },
-    { id: 3, title: 'Lesson 3' },
+    { id: 1, title: 'Alphabet', icon: icon1, link: '/lessons/1' },
+    { id: 2, title: 'Numbers', icon: icon2, link: '/lessons/2' },
+    { id: 3, title: 'Greetings', icon: icon3, link: '/lessons/3' },
   ];
 
+  const GoTo = (path: string) => {
+    navigate(path);
+  };
+
   return (
-    <div>
-      <h1>Lessons</h1>
-      <ul>
-        {lessons.map((lesson) => (
-          <li key={lesson.id}>
-            <Link to={`/lessons/${lesson.id}`}>{lesson.title}</Link>
-          </li>
-        ))}
-      </ul>
+    <div className='app-container bg'>
+      <div className='ui-container'>
+
+        <h2>Lessons</h2>
+        <div className='lessons-container'>
+          {lessons.map((lesson) => (
+            <GlassButton 
+              key={lesson.id}
+              className='lesson-card'
+              onClick={() => {GoTo(lesson.link)}}
+            >
+              <img src={lesson.icon} alt="icon"/>
+              <div>{lesson.title}</div>
+            </GlassButton>
+          ))}
+        </div>
+      </div>
+
     </div>
   );
 };
