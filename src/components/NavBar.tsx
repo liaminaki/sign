10d60ef2cd6a1de21c9efd2f8f1cb14352d6ebcb
Liam Iñaki Gillamac <yakogillamac96@gmail.com>
2025-03-16 22:53:53 +0800

    Add nav bar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+
+interface NavBarProps {
+  backPath: string;
+}
+
+const NavBar: React.FC<NavBarProps> = ({ backPath }) => {
+  const navigate = useNavigate();
+
+  const handleBackClick = () => {
+    navigate(backPath);
+  };
+
+  return (
+    <nav className='nav-bar'>
+      <div className='nav-bar-btns'>
+        <button title='Back' onClick={handleBackClick}>
+          <FontAwesomeIcon icon={faArrowLeft} size="lg" />
+        </button>
+        <Link to="/" className="home-button">
+          he<em>AR</em>
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
