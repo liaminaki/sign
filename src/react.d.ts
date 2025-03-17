04110814bdc5d38d90ad3cf48f3648e03762ca5b
Liam Iñaki Gillamac <yakogillamac96@gmail.com>
2025-03-18 02:24:49 +0800

    Set up model viewer

---
+++ src/react.d.ts
@@ -0,0 +1,10 @@
+import React from "react";
+import { ModelViewerElement } from "@google/model-viewer";
+
+export declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      "model-viewer": React.DetailedHTMLProps<Partial<ModelViewerElement>>;
+    }
+  }
+}
