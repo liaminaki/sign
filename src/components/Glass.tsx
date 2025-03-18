d12be39070b050be13d34a9df6e2e860d632cc16
Liam Iñaki Gillamac <yakogillamac96@gmail.com>
2025-03-18 21:20:45 +0800

    Add parameters in Glass Card component

--- src/components/Glass.tsx
+++ src/components/Glass.tsx
@@ -2,12 +2,14 @@ import React, { ReactNode } from 'react';
 import '../glass.css';
 
 type GlassProps = {
+    onClick?: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
     children: ReactNode;
+    className?: string;
 };
 
-const Glass: React.FC<GlassProps> = ({ children }) => {
+const Glass: React.FC<GlassProps> = ({ children, onClick, className }) => {
     return (
-        <div className="glass-card">
+        <div className={`glass-card ${className}`} onClick={onClick}>
             <div className="glass-stroke"></div>
             <div className="glass-content">{children}</div>
         </div>
