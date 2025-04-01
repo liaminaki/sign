71229c644f8e01efef3dff7ae1174cfd0ff69a22
Liam Iñaki Gillamac <yakogillamac96@gmail.com>
2025-04-02 00:37:38 +0800

    Init camkit only when camera app is opened

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,24 @@ import Home from './pages/Home';
 import Lessons from './pages/Lessons';
 import Lesson from './pages/Lesson';
 import CameraApp from './pages/CameraApp';
+import { CameraKit } from './contexts/CameraKitContext';
 
 function App() {
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/lessons" element={<Lessons />} />
         <Route path="/lessons/:id" element={<Lesson />} />
-        <Route path="/camera" element={<CameraApp />} />
+        <Route 
+          path="/camera" 
+          element={
+            <CameraKit>
+              <CameraApp />
+            </CameraKit>
+          } 
+        />
       </Routes>
     </Router>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { CameraKit } from './contexts/CameraKitContext.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <CameraKit>
-      <App />
-    </CameraKit>
+    <App />
   </React.StrictMode>
 );
