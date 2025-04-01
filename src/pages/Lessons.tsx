205f8a699c671f2d8dd351b7e7ac1fb798aa0651
Liam Iñaki Gillamac <yakogillamac96@gmail.com>
2025-04-01 19:21:51 +0800

    Use lessonData in Lessons

--- src/pages/Lessons.tsx
+++ src/pages/Lessons.tsx
@@ -2,22 +2,10 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import GlassButton from '../components/GlassButton';
 import NavBar from '../components/NavBar';
-import icon1 from '../assets/img/alphabet.svg';
-import icon2 from '../assets/img/numbers.svg';
-import icon3 from '../assets/img/greetings.svg';
+import lessonData from '../data/lessonData';
 
 const Lessons: React.FC = () => {
   const navigate = useNavigate();
-
-  const lessons = [
-    { id: 1, title: 'Alphabet', icon: icon1, link: '/lessons/1' },
-    { id: 2, title: 'Numbers', icon: icon2, link: '/lessons/2' },
-    { id: 3, title: 'Greetings', icon: icon3, link: '/lessons/3' },
-  ];
-
-  const GoTo = (path: string) => {
-    navigate(path);
-  };
 
   return (
     <main className='app-container bg'>
@@ -25,16 +13,16 @@ const Lessons: React.FC = () => {
       <section className='ui-container'>
         <h2>Lessons</h2>
         <div className='lessons-container'>
-          {lessons.map((lesson) => (
-            <GlassButton 
-              key={lesson.id}
-              className='card'
-              onClick={() => {GoTo(lesson.link)}}
-            >
-              <img src={lesson.icon} alt="icon"/>
-              <p>{lesson.title}</p>
-            </GlassButton>
-          ))}
+          {Object.values(lessonData).map((lesson) => (
+              <GlassButton 
+                key={lesson.id}
+                className='card'
+                onClick={() => navigate(`/lessons/${lesson.id}`)}
+              >
+                <img src={lesson.icon} alt={lesson.title} />
+                <p>{lesson.title}</p>
+              </GlassButton>
+            ))}
         </div>
       </section>
     </main>
