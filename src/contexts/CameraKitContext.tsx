605f3ec39a395c97fb30ec2ba3b9b260f97a45a8
Liam Iñaki Gillamac <yakogillamac96@gmail.com>
2025-04-09 16:55:34 +0800

    Use one final lens

--- src/contexts/CameraKitContext.tsx
+++ src/contexts/CameraKitContext.tsx
@@ -2,7 +2,11 @@ import { bootstrapCameraKit, CameraKitSession, Lens } from '@snap/camera-kit';
 import { createContext, useEffect, useRef, useState } from 'react';
 
 const apiToken = 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzM4NDkxMzU4LCJzdWIiOiJhMWZjNjVjYS1kYTExLTRhYTItYTIzMS0yNTY4NDg4NjI4NGV-U1RBR0lOR342MDdjNWQzZS0wOGU0LTRhYzEtYjFiZi0wY2M5ZGUwNmVlMTgifQ.uZpP9qeBFW7-8IVonL3izETfQwjQo0DHgmqpre08H3k';
-const lensGroupId = '3f933e66-7465-4caf-a58a-6f0473baa751';
+// const apiToken = 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzM4NDkxMzU4LCJzdWIiOiJhMWZjNjVjYS1kYTExLTRhYTItYTIzMS0yNTY4NDg4NjI4NGV-UFJPRFVDVElPTn44ZjMxY2E3Zi0yYTZhLTRiYTItODg0MS01YTE5N2M2YjA4ODcifQ.TwMGaBtp4EVt5oPfxGwOms4YuQDey3QkK2yCTJ_hIvc';
+// const lensGroupId = '28d9438b-081f-4cc4-8e1d-b4e71e1b072b';
+// const lensGroupId = '3f933e66-7465-4caf-a58a-6f0473baa751';
+// const lensGroupId = '1e6d9a7c-332e-4140-af2a-80f10098402b';
+const lensGroupId = 'be1f927d-db01-423f-8e21-7501a8a46210';
 
 export interface CameraKitState {
     session: CameraKitSession;
