f21406de693a91ae112091c03c8830a8d072afa4
Liam Iñaki Gillamac <yakogillamac96@gmail.com>
2025-06-08 04:54:08 +0800

    Update modal to support video

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Glass from './Glass';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
@@ -12,22 +12,48 @@ interface ModalProps {
 }
 
 const Modal: React.FC<ModalProps> = ({ onClose, src, title, onPractice }) => {
+    const [view, setView] = useState<'3d' | 'video'>('3d');
+
     return (
         <div className='modal-overlay' onClick={onClose}>
             <Glass className='modal-content' onClick={(e) => e.stopPropagation()}>
                 <button className='close-button' onClick={onClose}>
                     <FontAwesomeIcon icon={faXmark} size="lg" />
                 </button>
-                <model-viewer
-                    src={src}
-                    camera-controls
-                    autoplay
-                    // disable-zoom
-                    shadow-intensity="1"
-                    style={{ width: '100%', height: '300px' }}
-                    environment-image="legacy"
-                ></model-viewer>
-                <p>{title}</p>
+                {view === '3d' ? (
+                    <model-viewer
+                        src={src}
+                        camera-controls
+                        autoplay
+                        shadow-intensity="1"
+                        style={{ width: '90%', height: '300px' }}
+                        environment-image="legacy"
+                    ></model-viewer>
+                ) : (
+                    <iframe
+                        src={`https://drive.google.com/file/d/12zYLRzaKGMcxdyNJ6S9XrVJa9oK8Wnnx/preview`}
+                        width="70%"
+                        height="300"
+                        allow="autoplay"
+                        style={{ border: 0 }}
+                        title="Google Drive Video"
+                    ></iframe>
+                )}
+                <p className='modal-title'>{title}</p>
+                <div className="view-options">
+                    <button
+                        className={`sec-button${view === '3d' ? ' active' : ''}`}
+                        onClick={() => setView('3d')}
+                    >
+                        3D
+                    </button>
+                    <button
+                        className={`sec-button${view === 'video' ? ' active' : ''}`}
+                        onClick={() => setView('video')}
+                    >
+                        Video
+                    </button>
+                </div>
                 <button className='action-button' onClick={onPractice}>Practice</button>
             </Glass>
         </div>
